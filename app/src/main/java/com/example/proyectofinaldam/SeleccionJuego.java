12db6295fa7e0a770ee02fe8d0efd05f364d5e5a
pabloacedo04 <pabloacedo2004@gmail.com>
2024-04-07 18:38:28 +0200

    Activity de seleccion de juego

---
+++ app/src/main/java/com/example/proyectofinaldam/SeleccionJuego.java
@@ -0,0 +1,48 @@
+package com.example.proyectofinaldam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SeleccionJuego extends AppCompatActivity {
+
+    TextView prueba;
+    ImageButton juego1, juego2, juego3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccion_juego);
+
+        juego1 = findViewById(R.id.juego1);
+        juego2 = findViewById(R.id.juego2);
+        juego3 = findViewById(R.id.juego3);
+
+        prueba = findViewById(R.id.textView);
+        Bundle recibo = getIntent().getExtras();
+        String usuario = recibo.getString("usuario");
+
+        prueba.setText("Usuario: "+usuario);
+
+        juego1.setOnClickListener(view -> {
+            Toast.makeText(this, "Activity Juego 1", Toast.LENGTH_SHORT).show();
+        });
+
+        juego2.setOnClickListener(view -> {
+            Toast.makeText(this, "Activity Juego 2", Toast.LENGTH_SHORT).show();
+        });
+
+        juego3.setOnClickListener(view -> {
+            Toast.makeText(this, "Activity Juego 3", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
