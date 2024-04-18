674878c2ff151c26a9bf6b4ae1670c2420c73202
marccos21 <marcosiglesias1124@gmail.com>
2024-04-18 16:28:00 +0200

    añadido juego memory

--- app/src/main/java/com/example/proyectofinaldam/SeleccionJuego.java
+++ app/src/main/java/com/example/proyectofinaldam/SeleccionJuego.java
@@ -1,12 +1,13 @@
 package com.example.proyectofinaldam;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SeleccionJuego extends AppCompatActivity {
 
@@ -29,6 +30,8 @@ public class SeleccionJuego extends AppCompatActivity {
 
         juego1.setOnClickListener(view -> {
             Toast.makeText(this, "Activity Juego 1", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, memory.class);
+            startActivity(intent);
         });
 
         juego2.setOnClickListener(view -> {
