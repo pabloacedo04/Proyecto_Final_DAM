a56d80234b03d57e6db80884d7b0605c1cd1ea62
marccos21 <marcosiglesias1124@gmail.com>
2024-05-23 18:48:18 +0200

    interfaz tres en raya

--- app/src/main/java/com/example/proyectofinaldam/TresEnRayaGame.java
+++ app/src/main/java/com/example/proyectofinaldam/TresEnRayaGame.java
@@ -3,12 +3,10 @@ package com.example.proyectofinaldam;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 public class TresEnRayaGame extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tres_en_raya_game);
+
     }
 }
